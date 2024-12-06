df1ba9ab8468438bb30e7ade581baf90fc1aeed6
lebresilien <petitotapondjou@gmail.com>
2024-12-06 19:55:48 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,375 @@
-import Image from "next/image";
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils"
+import { CheckIcon } from "lucide-react";
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+
+type List = {
+  slug: string
+  name: string
+  selected: boolean
+}
+
+type Task = {
+  name: string
+  category: string
+  status: string
+  date: string
+}
+
+const navigationList = [
+  {
+    slug: 'messages',
+    name: 'messages',
+    selected: false
+  },
+  {
+    slug: 'today',
+    name: 'today\'s task',
+    selected: true
+  }, 
+  {
+    slug: 'activity',
+    name: 'last activity',
+    selected: false
+  }
+];
+
+const statusList = [
+  {
+    name: 'All',
+    default: true
+  },
+  {
+    name: 'Open',
+    default: false
+  },
+  {
+    name: 'Closed',
+
+    default: false
+  },
+  {
+    name: 'Archived',
+    default: false
+  }
+]
+
+const NavigationItem = ({
+  slug, 
+  name, 
+  selected,
+  list,
+  setList,
+  setCurrentDisplay
+}: {
+  slug: string, 
+  name: string, 
+  selected: boolean,
+  list: List[],
+  setList: (list: List[]) => void,
+  setCurrentDisplay: (value: string) => void,
+}) => {
+
+  const onPress = (slug: string) => {
+
+    const copyList = [...list];
+
+    const currentSelectedItem = copyList.find((item) => item.slug === slug);
+    const otherItem = copyList.filter((item) => item.slug !== slug);
+   
+    if(currentSelectedItem && otherItem) {
+      currentSelectedItem.selected = true;
+      for(const item of otherItem) {
+        item.selected = false;
+      }
+      setList(copyList);
+      setCurrentDisplay(slug);
+    }
+  }
+
+  return (
+    <div 
+      className={cn("border-b-2 border-b-secondary p-5", selected && "border-b-black")}
+      onClick={() => onPress(slug)}
+    >
+      <span className={cn(
+        "capitalize cursor-pointer", 
+        selected && "font-bold",
+        !selected && "hover:font-light"
+      )}
+      >
+        {name}
+      </span> 
+    </div>
+  )
+}
+
+const Navigation = ({ 
+  list, 
+  setList,
+  setCurrentDisplay
+}: {
+  list: List[], 
+  setList: (list: List[]) => void,
+  setCurrentDisplay: (value: string) => void,
+}) => {
+  return (
+    <div className="flex flex-row justify-space-between aligns-center">
+      {
+        list.map((item) => (
+          <NavigationItem 
+            key={item.slug}
+            slug={item.slug}
+            name={item.name}
+            selected={item.selected}
+            list={list}
+            setList={setList}
+            setCurrentDisplay={setCurrentDisplay}            
+          />
+        ))
+      }
+    </div>
+  )
+}
+
+const Checkbox = ({ checked }: ({ checked: boolean })) => (
+  <div className="relative">
+    <div 
+      className={cn(
+        "flex h-8 w-8 rounded-full",
+        checked ? "bg-primary" : "border-muted border-2"
+      )}
+    ></div>
+    {checked && ((
+      <div className="absolute inset-0 left-1 top-1" onClick={() => {} }>
+        <CheckIcon className="h-6 w-6 text-white" />
+      </div>
+    ))}
+  </div>
+)
+
+const TaskCard = ({ name, category, date, status, tasks, setTasks } : {
+  name: string,
+  category: string,
+  date: string,
+  status: string,
+  tasks: Task[],
+  setTasks: (t: Task[]) => void
+}) => {
+  const closeTask = (name: string) => {
+      const cop = [...tasks];
+      const result = cop.find((item) => item.name === name);
+      console.log('yee', result)
+      if(result) {
+        result.status = "Closed";
+        setTasks(cop);
+      }
+  }
+
+  return (
+    <div className="flex flex-col bg-white p-5 rounded-lg cursor-pointer">
+      
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col">
+          <span className="text-xl tracking-wider font-black">{name}</span>
+          <span className="text-muted">{category}</span>
+        </div>
+        <div onClick={() => closeTask(name)}>
+          <Checkbox checked={status === "Closed" ? true : false} />
+        </div>
+      </div>
+
+      <Separator className="my-4"/>
+
+      <div className="flex justify-between items-center">
+        <span className="text-muted">{date} </span>
+        <div className="">vieueueueu</div>
+      </div>
+      
+    </div>
+  )
+}
+
+const DialogForm = ({ 
+  tasks, 
+  setTasks
+}: { 
+  tasks: Task[], 
+  setTasks: (t: Task[]) => void  
+}) => {
+
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const date = new Date();
+
+  const save = () => {
+  
+    setTasks([
+      ...tasks,
+      {
+        name: name,
+        category: category,
+        date: date.toLocaleDateString(),
+        status: ''
+      }
+    ]);
+    setName('');
+
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <span className="text-primary tracking-wide capitalize group-hover:font-light">+ new task</span>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+          <DialogDescription>
+            Fill the fields to create a new tasks.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input value={name} placeholder="Add" className="col-span-3" onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Category
+            </Label>
+            <Select onValueChange={(value) => setCategory(value)}>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Dev" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="Dev">Dev</SelectItem>
+                <SelectItem value="Devops">Devops</SelectItem>
+                <SelectItem value="ci/cd">ci/cd</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button onClick={save}>Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  ) 
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  
+  const [listNav, setListNav] = useState<List[]>(navigationList);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [currentDisplay, setCurrentDisplay] = useState('messages');
+  
+  const status = statusList;
+  const today = new Date();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const formatDate = (date: Date) => {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const dayOfMonth = date.getDate();
+    const month = months[date.getMonth()];
+  
+    return `${dayOfWeek}, ${dayOfMonth} ${month}`;
+  }
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-secondary">
+      <main className="flex flex-col row-start-2 items-center sm:items-start bg-white rounded-lg p-10">
+        
+        <Navigation 
+          list={listNav} 
+          setList={setListNav} 
+          setCurrentDisplay={setCurrentDisplay}
+        />
+
+        <div className="w-full bg-secondary py-10 px-5">
+          {currentDisplay === "messages" && ((
+            <div className="">messages</div>
+          ))}
+          {currentDisplay === "today" && ((
+            <div className="flex flex-col gap-8">
+
+              <div className="w-full flex flex-row justify-between items-center">
+                <div className="flex flex-col gap-1">
+                  <span className="text-xl capitalize font-bold tracking-wide">today&apos;s task</span>
+                  <span className="text-muted">{formatDate(today)}</span>
+                </div>
+                <div className="flex rounded-full p-2 items-center bg-blue-300 cursor-pointer group">
+                  <DialogForm tasks={tasks} setTasks={setTasks} />
+                </div>
+              </div>
+
+              <div className="flex justify-between items-center gap-5">
+                {status.map((item, index) => (
+                  <div 
+                    className="flex items-center justify-center space-x-2 cursor-pointer group" 
+                    key={index}
+                  >
+                    <span className={cn("group-hover:font-light", item.default && "text-primary")}>{item.name}</span>
+                    <Badge
+                      className={cn("text-white", !item.default && "bg-muted")}
+                      variant={!item.default ? "secondary" : "default"}
+                    >
+                      {item.name === "All" && tasks.length}
+                      {item.name === "Open" && ((
+                        tasks.filter((item) => item.status === "Open").length
+                      ))}
+                      {item.name === "Closed" && tasks.filter((item) => item.status === "Closed").length}
+                      {item.name === "Archived" && tasks.filter((item) => item.status === "Archived").length}
+                    </Badge>
+                  </div>
+                ))}
+              </div>
+
+              {tasks.map((item, index) => (
+                <TaskCard
+                  name={item.name}
+                  category={item.category}
+                  status={item.status}
+                  date={item.date}
+                  key={index}
+                  tasks={tasks}
+                  setTasks={setTasks}
+                />
+              ))}
+
+            </div>
+          ))}
+          {currentDisplay === "activity" && ((
+            <div className="">Activity</div>
+          ))}
         </div>
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
